ede4a3dcece73686d7e11d1f3e83746562994446
Jonathan Hussey <jhusseydesign@gmail.com>
2018-08-18 22:26:12 +0100

    Added red wine quality prediction example

---
+++ opticverge/examples/machine_learning/regression/red_wine_quality/main.py
@@ -0,0 +1,42 @@
+from opticverge.core.enum.policy import Policy
+from opticverge.core.log.logger import application_logger
+from opticverge.core.solver.generic_ais import AIS
+from opticverge.examples.machine_learning.regression.red_wine_quality.problem import RedWineQualityPredictionProblem
+from opticverge.external.scikit.chromosome.regression.ensemble import GradientBoostingRegressorChromosome, \
+    XGBRegressorChromosome
+from opticverge.external.scikit.chromosome.regression.neighbor import KNeighborsRegressorChromosome
+from opticverge.external.scikit.chromosome.regression.perceptron import MLPRegressorChromosome
+from opticverge.external.scikit.enum.normaliser import Normaliser
+from opticverge.external.scikit.enum.scoring_function import Scoring
+
+
+def run():
+    try:
+        problem = RedWineQualityPredictionProblem(
+            scoring_function=Scoring.MeanAbsoluteError,
+            normaliser=Normaliser.RobustScaler,
+            folds=4
+        )
+
+        solver = AIS(
+            chromosome=XGBRegressorChromosome(),
+            problem=problem,
+            population_size=20,
+            epochs=-1,
+            policies=[
+                Policy.EnforceLimitedMutationAttempts,
+                Policy.EnforceUniqueChromosome
+            ],
+            duration=None
+        )
+
+        best_chromosome = solver.run()
+
+        problem.log_chromosome(best_chromosome, solver)
+
+    except Exception as ex:
+        application_logger.exception(msg="", exc_info=ex)
+
+
+if __name__ == '__main__':
+    run()
