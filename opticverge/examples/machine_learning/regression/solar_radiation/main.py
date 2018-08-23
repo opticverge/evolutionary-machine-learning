754bf4a5cfe54cba5b811f936bc7a70d10ba6004
Jonathan Hussey <jhusseydesign@gmail.com>
2018-08-23 06:09:36 +0100

    Added solar radiation prediction problem

---
+++ opticverge/examples/machine_learning/regression/solar_radiation/main.py
@@ -0,0 +1,53 @@
+from opticverge.core.enum.policy import Policy
+from opticverge.core.log.logger import application_logger
+from opticverge.core.solver.generic_ais import AIS
+from opticverge.examples.machine_learning.regression.solar_radiation.problem import SolarRadiationPredictionProblem
+from opticverge.external.scikit.chromosome.regression.ensemble import GradientBoostingRegressorChromosome, \
+    XGBRegressorChromosome, RandomForestRegressorChromosome, AdaBoostRegressorChromosome
+from opticverge.external.scikit.chromosome.regression.neighbor import KNeighborsRegressorChromosome
+from opticverge.external.scikit.chromosome.regression.perceptron import MLPRegressorChromosome
+from opticverge.external.scikit.chromosome.regression.tree import DecisionTreeRegressorChromosome
+from opticverge.external.scikit.enum.normaliser import Normaliser
+from opticverge.external.scikit.enum.scoring_function import Scoring
+
+
+def run():
+    try:
+        problem = SolarRadiationPredictionProblem(
+            scoring_function=Scoring.MeanAbsoluteError,
+            normaliser=Normaliser.RobustScaler,
+            folds=3
+        )
+
+        """
+        Regressor choices:
+        - AdaBoostRegressorChromosome
+        - DecisionTreeRegressorChromosome
+        - GradientBoostingRegressorChromosome
+        - KNeighborsRegressorChromosome                                                                                                                                                                                                                                                         
+        - MLPRegressorChromosome
+        - RandomForestRegressorChromosome
+        - XGBRegressorChromosome
+        """
+        solver = AIS(
+            chromosome=GradientBoostingRegressorChromosome(),
+            problem=problem,
+            population_size=20,
+            epochs=-1,
+            policies=[
+                Policy.EnforceLimitedMutationAttempts,
+                Policy.EnforceUniqueChromosome
+            ],
+            duration=None
+        )
+
+        best_chromosome = solver.run()
+
+        problem.log_chromosome(best_chromosome, solver)
+
+    except Exception as ex:
+        application_logger.exception(msg="", exc_info=ex)
+
+
+if __name__ == '__main__':
+    run()
---
+++ opticverge/examples/machine_learning/regression/solar_radiation/problem.py
@@ -0,0 +1,47 @@
+from typing import Any, Dict
+
+import pandas
+import numpy as np
+from sklearn import datasets
+
+from opticverge.core.chromosome.abstract_chromosome import AbstractChromosome
+from opticverge.core.enum.objective import Objective
+from opticverge.core.log.logger import data_logger, DATA
+from opticverge.core.solver.abstract_solver import AbstractSolver
+from opticverge.external.scikit.enum.normaliser import Normaliser
+from opticverge.external.scikit.enum.scoring_function import Scoring
+from opticverge.external.scikit.problem.abstract_regression_problem import AbstractRegressionProblem
+
+
+class SolarRadiationPredictionProblem(AbstractRegressionProblem):
+    def __init__(self, scoring_function: Scoring, normaliser: Normaliser = None, folds: int = 1):
+
+        df = pandas.read_csv("./SolarPrediction.csv", sep=",", usecols=[
+            "Temperature", "Pressure", "Humidity", "WindDirection(Degrees)", "Speed", "Radiation"
+        ])
+
+        data = np.array(df[["Temperature", "Pressure", "Humidity", "WindDirection(Degrees)", "Speed"]])
+        target = np.ndarray.flatten(np.array(df[["Radiation"]]))
+
+        super(SolarRadiationPredictionProblem, self).__init__(
+            Objective.Minimisation,
+            "Solar Radiation Prediction",
+            data_x=data,
+            target_x=target,
+            normaliser=normaliser,
+            folds=folds,
+            scoring_function=scoring_function
+        )
+
+    def log_chromosome(self, chromosome: AbstractChromosome, solver: AbstractSolver,
+                       additional_data: Dict[str, Any] = None, separator="|"):
+        data_str = super(SolarRadiationPredictionProblem, self).log_chromosome(
+            chromosome,
+            solver,
+            None
+        )
+
+        data_logger.log(DATA, data_str)
+
+    def objective_function(self, chromosome: AbstractChromosome):
+        super(SolarRadiationPredictionProblem, self).objective_function(chromosome)
