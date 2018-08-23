2bd5b6611bc26eeeeca03121402c9f5c7cea9e49
Jonathan Hussey <jhusseydesign@gmail.com>
2018-08-23 06:04:09 +0100

    Increased default sample size
    Enables better exploration during the evolutionary process

--- opticverge/core/globals.py
+++ opticverge/core/globals.py
@@ -1,5 +1,5 @@
 """ The default sample size for all distributions """
-DEFAULT_SAMPLE_SIZE = 1
+DEFAULT_SAMPLE_SIZE = 10
 
 """ The max value within the int32 number space """
 INT32_MAX = (2**31) - 1
